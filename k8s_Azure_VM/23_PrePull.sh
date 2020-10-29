bdfc234b713e9d4448e4d0e17ad084737e490513
Ben Weissman <b.weissman@solisyon.de>
2020-10-29 14:11:55 +0100

    Update 23_PrePull.sh

--- k8s_Azure_VM/23_PrePull.sh
+++ k8s_Azure_VM/23_PrePull.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 export DOCKER_REGISTRY="mcr.microsoft.com"
 export DOCKER_REPOSITORY="mssql/bdc"
 export DOCKER_TAG="2019-CU8-ubuntu-16.04"
-
+export ARCDOCKER_TAG="latest"
 IMAGES=(
 	mssql-app-service-proxy
         mssql-control-watchdog
@@ -34,6 +34,22 @@ CUS=(
 	CU6
 	CU8
 )
+ARCIMAGES=(
+	arc-sqlmi
+	arc-controller
+	mcr.microsoft.com/arcdata/arc-monitor-fluentbit
+	mcr.microsoft.com/arcdata/arc-monitor-telegraf
+	mcr.microsoft.com/arcdata/arc-monitor-collectd      
+	mcr.microsoft.com/arcdata/arc-postgres-12
+	mcr.microsoft.com/arcdata/arc-server-controller
+	mcr.microsoft.com/arcdata/arc-service-proxy
+	mcr.microsoft.com/arcdata/arc-bootstrapper
+	mcr.microsoft.com/arcdata/arc-control-watchdog
+	mcr.microsoft.com/arcdata/arc-monitor-kibana
+	mcr.microsoft.com/arcdata/arc-monitor-elasticsearch
+	mcr.microsoft.com/arcdata/arc-monitor-grafana
+	mcr.microsoft.com/arcdata/arc-monitor-influxdb            
+)
 for cu in "${CUS[@]}";
 do
 	docker pull $DOCKER_REGISTRY/mssql/server:2019-$cu-ubuntu-18.04
@@ -42,3 +58,8 @@ for image in "${IMAGES[@]}";
 do
     docker pull $DOCKER_REGISTRY/$DOCKER_REPOSITORY/$image:$DOCKER_TAG
 done
+for arcimage in "${IMAGES[@]}";
+do
+    docker pull $DOCKER_REGISTRY/arcdata/$image:$ARCDOCKER_TAG
+done
+
