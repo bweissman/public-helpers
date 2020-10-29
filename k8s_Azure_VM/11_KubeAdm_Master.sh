757411d367d2ad0253046af25aa492974227e998
Ben Weissman <b.weissman@solisyon.de>
2020-10-29 08:00:42 +0100

    a

--- k8s_Azure_VM/11_KubeAdm_Master.sh
+++ k8s_Azure_VM/11_KubeAdm_Master.sh
@@ -3,11 +3,12 @@
 # Initialize a kubernetes cluster on the current node.
 #
 KUBE_VERSION=1.16.3
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=$KUBE_VERSION
+kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=$KUBE_VERSION
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown 1000:1000 $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl apply -f rbac.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
 kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
+kubeadm token create --print-join-command > agent_join.sh
