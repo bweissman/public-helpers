a9bd8936e25f872d760aa1a34dc37f6cbefc5547
Ben Weissman <b.weissman@solisyon.de>
2020-10-29 08:08:10 +0100

    Create 22_KubeAdm_JoinAgent.sh

---
+++ k8s_Azure_VM/22_KubeAdm_JoinAgent.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo y | pscp -pw Passw0rdPassw0rd -l precon 10.10.1.4:/home/precon/.kube/config /home/precon/.kube
+pscp -pw Passw0rdPassw0rd -l precon 10.10.1.4:/home/precon/agent_join.sh 31_Agent_join.sh
+chmod +x 31_Agent_join.sh
+sudo ./31_Agent_join.sh
