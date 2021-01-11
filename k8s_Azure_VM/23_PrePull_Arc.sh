db13aec525f496078ab2807cfc651fe914f09b13
Ben Weissman <b.weissman@solisyon.de>
2021-01-11 11:30:02 +0100

    Create 23_PrePull_Arc.sh

---
+++ k8s_Azure_VM/23_PrePull_Arc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -Eeuo pipefail
+export DOCKER_REGISTRY="mcr.microsoft.com"
+export DOCKER_REPOSITORY="mssql/bdc"
+export ARCDOCKER_TAG="latest"
+ARCIMAGES=(
+	arc-sqlmi
+	arc-controller
+	arc-monitor-fluentbit
+	arc-monitor-telegraf
+	arc-monitor-collectd      
+	arc-postgres-12
+	arc-server-controller
+	arc-service-proxy
+	arc-bootstrapper
+	arc-control-watchdog
+	arc-monitor-kibana
+	arc-monitor-elasticsearch
+	arc-monitor-grafana
+	arc-monitor-influxdb            
+)
+for arcimage in "${ARCIMAGES[@]}";
+do
+    docker pull $DOCKER_REGISTRY/arcdata/$arcimage:$ARCDOCKER_TAG
+done
+
